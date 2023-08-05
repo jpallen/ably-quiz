7761ff360b3d434beac31290448194694c4ddb38
James Allen <james@jpallen.net>
2023-08-05 13:45:07 +0100

    Put in basic quiz lifecycle

--- src/server.ts
+++ src/server.ts
@@ -16,7 +16,7 @@ const ably = new Ably.Rest({ key: ABLY_API_KEY })
 
 app.post('/client/token', async (req, res) => {
   const token = await ably.auth.requestToken({ clientId: 'foo' })
-  console.log('Token requested', token)
+  console.log('Token requested')
   res.json(token)
 })
 
@@ -26,8 +26,83 @@ app.listen(PORT, HOST, () => {
   console.log(`Server listening on ${HOST}:${PORT}`)
 })
 
-const realtime = new Ably.Realtime({ key: ABLY_API_KEY })
-const channel = realtime.channels.get('quiz')
-channel.presence.subscribe('enter', (member) => {
-  console.log(member)
-})
+class Quiz {
+  minClientCount = 1
+  channelName = 'quiz'
+  state: 'waiting' | 'running' | 'finished' = 'waiting'
+  channel: Ably.Types.RealtimeChannelPromise
+  questions = [
+    {
+      question: 'Question 1',
+      options: ['Correct', 'Wrong', 'Incorrect'],
+      answerIndex: 0
+    },
+    {
+      question: 'Question 2',
+      options: ['Wrong', 'Correct', 'Incorrect'],
+      answerIndex: 1
+    }
+  ]
+
+  nextQuestionIndex = 0
+  timePerQuestion = 5000 // ms
+
+  constructor() {
+    const realtime = new Ably.Realtime({ key: ABLY_API_KEY })
+    this.channel = realtime.channels.get(this.channelName)
+    this.channel.presence.subscribe('enter', () => {
+      console.log('Client connected')
+      this.startQuizIfEnoughClients()
+    })
+  }
+
+  async startQuizIfEnoughClients() {
+    if (this.state !== 'waiting') return
+    const presence = await this.channel.presence.get()
+    const clientCount = presence.length
+    console.log(`${clientCount} clients connected`)
+    if (clientCount >= this.minClientCount) {
+      this.startQuiz()
+    }
+  }
+
+  startQuiz() {
+    if (this.state !== 'waiting') return
+    console.log('Starting quiz')
+    this.state = 'running'
+    this.askNextQuestion()
+  }
+
+  askNextQuestion() {
+    if (this.state !== 'running') return
+
+    const currentQuestionIndex = this.nextQuestionIndex
+    const question = this.questions[this.nextQuestionIndex]
+    this.nextQuestionIndex++
+
+    if (!question) {
+      // We've run out of questions
+      this.finishQuiz()
+      return
+    }
+
+    console.log('Asking question', question)
+    this.channel.publish('question', {
+      questionId: currentQuestionIndex,
+      question: question.question,
+      options: question.options
+    })
+
+    setTimeout(() => {
+      this.askNextQuestion()
+    }, this.timePerQuestion)
+  }
+
+  finishQuiz() {
+    if (this.state !== 'running') return
+    console.log('finishing quiz')
+    this.state = 'finished'
+  }
+}
+
+const quiz = new Quiz()
